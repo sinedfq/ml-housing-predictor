debb4c7748a3d8d27387146803cd996c4fc73050
Хухарев Денис <deniskhuharev@gmail.com>
2026-07-29 14:29:05 +0700

    Add new test data

--- tests/test_api.py
+++ tests/test_api.py
@@ -3,12 +3,17 @@ from api.main import app
 
 client = TestClient(app)
 test_data = {
-        "total_area": 54.5,
-        "rooms": 2,
-        "to_center_km": 8.3,
-        "metro_distance_min": 7,
-        "district": "Zayeltsovsky"
-    }
+    "total_area": 54.5,
+    "rooms": 2,
+    "to_center_km": 8.3,
+    "metro_distance_min": 7,
+    "district": "Zayeltsovsky",
+    "floor": 5,
+    "total_floors": 17,
+    "developer": "PIK", 
+    "complex_class": "comfort",
+    "okrug": "Central"
+}
 
 def test_predict_price():
     response = client.post("/predict", json=test_data)
